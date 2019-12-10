7d0bb014b7e31d158162057ebc807cdaf267c64c
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-12-10 20:13:27 -0300

    recebe parâmetro de caminho

--- scripts/fetch_all_data.py
+++ scripts/fetch_all_data.py
@@ -2,45 +2,47 @@ import sys, os
 
 PYTHON_VERSION = 'python3.6'
 
-def call_action_year(action, year):
+def call_action_year(action, year, path):
     '''
     Seleciona qual acao será retornada.
     '''
     if action.lower() == 'contrato':
-        call_fetch(year, 'fetch_contratos.py')
+        call_fetch(year, 'fetch_contratos.py', path)
         
     elif action.lower() == 'licitacao':
-        call_fetch(year, 'fetch_licitacoes.py')
+        call_fetch(year, 'fetch_licitacoes.py', path)
         
     elif action.lower() == 'empenho':
-        call_fetch(year, 'fetch_empenhos.py')
+        call_fetch(year, 'fetch_empenhos.py', path)
         
-def call_fetch(year, call):
+def call_fetch(year, call, path):
     '''
     Requesita arquivo de acao de acordo com o(s) ano(s) solicitado(s).
     '''
     for y in year:
-        os.system(PYTHON_VERSION + ' ' + call + ' ' + y)
+        os.system(PYTHON_VERSION + ' ' + call + ' ' + y + ' ' + path)
 
-def call_all_fetch(year):
+def call_all_fetch(year, path):
     '''
     Requisita todos os arquivos de baixar dados.
     '''
     for y in year:
-        os.system(PYTHON_VERSION + ' fetch_contratos.py '+ y)
-        os.system(PYTHON_VERSION + ' fetch_empenhos.py '+ y)
-        os.system(PYTHON_VERSION + ' fetch_licitacoes.py '+ y)
+        os.system(PYTHON_VERSION + ' fetch_contratos.py '+ y + ' ' + path)
+        os.system(PYTHON_VERSION + ' fetch_empenhos.py '+ y + ' ' + path)
+        os.system(PYTHON_VERSION + ' fetch_licitacoes.py '+ y + ' ' + path)
 
 if __name__ == "__main__":
     in_action = str(sys.argv[1])
     in_year = str(sys.argv[2])
+    path = str(sys.argv[3])
+
     in_year = in_year.split(',')
 
     if in_action.lower() == 'all':
-        call_all_fetch(in_year)
+        call_all_fetch(in_year, path)
             
     else:
         in_action = in_action.split(',')
         for a in in_action:
-            call_action_year(a, in_year)
+            call_action_year(a, in_year, path)
     
