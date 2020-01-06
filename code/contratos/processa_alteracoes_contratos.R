da390ce7d78210e31e3254f44472b0a0f2ac52fc
gileadekelvin <gileadekelvin@gmail.com>
2020-01-06 11:55:46 -0300

    Adiciona processamento da tabela de alterações de contratos

---
+++ code/contratos/processa_alteracoes_contratos.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(here)
+library(janitor)
+
+#' Processa dados de alteração de contratos do estado do Rio Grande do Sul para um conjunto de anos
+#' 
+#' @param anos Vector de inteiros com anos para captura das alterações dos contratos
+#' 
+#' @return Dataframe com informações das alterações dos contratos
+#' 
+#' @examples 
+#' alteracoes_contratos <- import_alteracoes_contratos(c(2017, 2018, 2019))
+#' 
+import_alteracoes_contratos <- function(anos = c(2017, 2018, 2019)) {
+  
+  alteracoes_contratos <- pmap_dfr(list(anos),
+                        ~ import_alteracoes_contratos_por_ano(..1)
+  )
+  
+  return(alteracoes_contratos)
+}
+
+#' Importa dados de contratos em um ano específico para o estado do Rio Grande do Sul
+#' 
+#' @param ano Inteiro com o ano para recuperação das alterações dos contratos
+#'
+#' @return Dataframe com informações das alterações dos contratos
+#'   
+#' @examples 
+#' alteracoes_contratos <- import_alteracoes_contratos_por_ano(2019)
+#' 
+import_alteracoes_contratos_por_ano <- function(ano = 2017) {
+  message(paste0("Importando alterações dos contratos do ano ", ano))
+
+  ## Limpando dados 
+  file_path <- here(paste0("data/contratos/", ano, "/alteracao.csv"))
+  writeLines(iconv(readLines(file_path, skipNul = TRUE)), file_path)
+  
+  ## Lendo dados tratados
+  alteracoes_contratos <- read_csv(file_path, 
+                                   col_types = cols(.default = "c"))
+  
+  return(alteracoes_contratos)
+}
+
+processa_info_alteracoes_contratos <- function(anos = c(2017, 2018, 2019)) {
+  source(here("code/contratos/processa_contratos.R"))
+  source(here("code/contratos/processa_tipos_alteracao_contrato.R"))
+  
+  contratos_merenda <- processa_info_contratos(anos) %>% 
+    select(id_orgao, nr_licitacao, ano_licitacao, nr_contrato, ano_contrato)
+  
+  alteracoes_contratos <- import_alteracoes_contratos(anos)
+  
+  tipo_operacao_alteracao <- processa_tipos_alteracao_contrato()
+  
+  info_alteracoes_contrato <- alteracoes_contratos %>%
+    inner_join(contratos_merenda, 
+               by = c("CD_ORGAO" = "id_orgao", "NR_LICITACAO" = "nr_licitacao", 
+                      "ANO_LICITACAO" = "ano_licitacao", "NR_CONTRATO" = "nr_contrato",
+                      "ANO_CONTRATO" = "ano_contrato")) %>%
+    clean_names() %>%
+    select(cd_orgao, ano_licitacao, nr_licitacao, nr_contrato, ano_contrato, 
+           sq_evento, cd_tipo_operacao, nr_dias_novo_prazo, vl_acrescimo, vl_reducao, pc_acrescimo, 
+           pc_reducao, ds_justificativa) %>%
+    left_join(tipo_operacao_alteracao, by = c("cd_tipo_operacao")) %>%
+    rename(id_orgao = cd_orgao,
+           id_evento_contrato = sq_evento,
+           vigencia_novo_contrato = nr_dias_novo_prazo,
+           motivo_alteracao_contrato = tipo_operacao_alteracao) %>%
+    select(-c(cd_tipo_operacao))
+  
+  return(info_alteracoes_contrato)
+}
+
