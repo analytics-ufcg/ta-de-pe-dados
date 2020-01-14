71143cebd1a996e69a2dadaf4b5003fa73c1b576
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2020-01-14 15:01:30 -0300

    Documenta funções de processamento da tabela de itens

--- code/licitacoes/processa_itens.R
+++ code/licitacoes/processa_itens.R
@@ -1,4 +1,6 @@
-
+#' Renomeia as colunas repetidas do dataframe de itens
+#' @param itens Dataframe de itens das licitações
+#' @return Dataframe com nome das colunas de acordo Manual do leiaute do e-Validador 
 rename_duplicate_columns <- function(itens) {
   names(itens)[names(itens) == 'TP_DOCUMENTO'] <- 'TP_DOCUMENTO_VENCEDOR'
   names(itens)[names(itens) == 'NR_DOCUMENTO'] <- 'NR_DOCUMENTO_VENCEDOR'
@@ -7,11 +9,22 @@ rename_duplicate_columns <- function(itens) {
   itens
 }
 
-generate_id <- function(itens, ano) {
-  itens$ID <- paste0(ano, seq.int(nrow(itens)))
-  itens
+
+#' Gera um identificador único para cada registro do dataframe
+#' @param itens Dataframe sem identificador único
+#' @param ano Inteiro com o ano para uso na criação do identificador
+#' @return Dataframe com identificador único
+generate_id <- function(df, ano) {
+  df$ID <- paste0(ano, seq.int(nrow(df)))
+  df
 }
 
+
+#' Importa itens das licitações de um ano específico para o estado do Rio Grande do Sul
+#' @param ano Inteiro com o ano para recuperação dos itens
+#' @return Dataframe com informações dos itens das licitações
+#' @examples 
+#' itens <- import_itens_licitacao_por_ano(2019)
 import_itens_licitacao_por_ano <- function(ano) {
   message(paste0("Importando itens das licitações do ano ", ano))
   
