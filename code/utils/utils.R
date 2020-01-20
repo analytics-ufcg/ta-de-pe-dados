825a746f3d27d31fe8d72b3736bf6718026dbc1c
gileadekelvin <gileadekelvin@gmail.com>
2020-01-20 14:52:50 -0300

    Processa dados de todos os concorrentes em licitações no Rio Grande do Sul

--- code/utils/utils.R
+++ code/utils/utils.R
@@ -2,8 +2,10 @@
 #' @param df Dataframe sem identificador único
 #' @param ano Inteiro com o ano para uso na criação do identificador
 #' @param constante Identificador da tabela
+#' @param id Nome da coluna do identificador
 #' @return Dataframe com identificador único
-generate_id <- function(df, ano, constante) {
-  df$ID <- paste0(constante, ano, seq.int(nrow(df)))
+generate_id <- function(df, ano, constante, id) {
+  df <- df %>% 
+    mutate(!!id := paste0(constante, get(ano), seq.int(nrow(df))))
   df
 }
