30092fec9e556fe32fab17b191997115ad4589d3
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2020-01-22 14:58:04 -0300

    Adiciona tabela de orgao

---
+++ code/orgaos/processa_orgaos.R
@@ -0,0 +1,15 @@
+#' Cria dataframe com informações dos Municípios participantes de licitações
+#' 
+#' @examples 
+#' municipios <- processa_info_municipios()
+#' 
+processa_info_orgaos <- function(licitacoes_df) {
+  
+  info_orgaos <- licitacoes_df %>%
+    janitor::clean_names() %>% 
+    dplyr::distinct(nm_orgao, cd_orgao) %>%
+    dplyr::mutate(id_estado = "43") %>% 
+    dplyr:: select(orgao_id = cd_orgao, id_estado, nm_orgao)
+  
+  return(info_orgaos)
+}
