b3fea3707066c34cd308f44a0e39f381dccb3f3f
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2020-03-02 11:20:07 -0300

    adiciona linha de id contrato no create de novidade e no update

--- feed/scripts/create/create_novidade.sql
+++ feed/scripts/create/create_novidade.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS "novidade" (
     "id_original" BIGINT,
     "nome_municipio" VARCHAR(30),
     "texto_novidade" VARCHAR(20),
+    "id_contrato" INTEGER,
     PRIMARY KEY("id_novidade"),
     FOREIGN KEY("id_licitacao") REFERENCES licitacao("id_licitacao"),
     FOREIGN KEY("id_tipo") REFERENCES tipo_novidade("id_tipo")
--- feed/scripts/update/update_novidade.sql
+++ feed/scripts/update/update_novidade.sql
@@ -15,6 +15,8 @@ DO
     id_licitacao = EXCLUDED.id_licitacao,
     data = EXCLUDED.data,
     id_original = EXCLUDED.id_original,
+    id_contrato = EXCLUDED.id_contrato,
+    texto_novidade = EXCLUDED.texto_novidade,
     nome_municipio = EXCLUDED.nome_municipio;
 
 DROP TABLE temp_novidade;
