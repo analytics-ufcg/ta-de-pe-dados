d13ba24c38d5dd36c96c8995a233e605f5e844d1
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2020-12-03 12:54:25 -0300

    Cria DAO TCE PE e script que busca e escreve dados

---
+++ code/fetcher/DAO_TCE_PE.R
@@ -0,0 +1,152 @@
+#' @title Busca dados das licitações do TCE-PE
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @param limite_inferior Data limite inferior do ano das licitações
+#' @param limite_superior Data limite superior do ano das licitações
+#' @return Dataframe contendo informações sobre as licitações
+#' @rdname fetch_licitacoes_pe
+#' @examples
+#' licitacoes_pe <- fetch_licitacoes_pe(tce_bd_con)
+fetch_licitacoes_pe <- function(tce_bd_con, limite_inferior = 2017, limite_superior = 2020) {
+  
+  licitacoes <- tibble::tibble()
+  tryCatch({
+    licitacoes <- DBI::dbGetQuery(tce_bd_con, sprintf(
+      "SELECT * FROM SCA_LicitacoesDetalhes WHERE AnoProcesso >= %d AND AnoProcesso <= %d;", limite_inferior, limite_superior))
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados das licitações (SQL Server): ", e))
+  )
+  
+  return(licitacoes)
+}
+
+#' @title Busca dados dos contratos do TCE-PE
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @param limite_inferior Data limite inferior do ano dos contratos
+#' @param limite_superior Data limite superior do ano dos contratos
+#' @return Dataframe contendo informações sobre dos contratos
+#' @rdname fetch_contratos_pe
+#' @examples
+#' contratos_pe <- fetch_contratos_pe(tce_bd_con)
+fetch_contratos_pe <- function(tce_bd_con, limite_inferior = 2017, limite_superior = 2020) {
+  
+  contratos <- tibble::tibble()
+  tryCatch({
+    contratos <- DBI::dbGetQuery(tce_bd_con, sprintf(
+      "SELECT * FROM Contratos WHERE AnoContrato >= %d AND AnoContrato <= %d;", limite_inferior, limite_superior))
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados dos contratos (SQL Server): ", e))
+  )
+  
+  return(contratos)
+}
+
+#' @title Busca dados dos aditivos dos contratos do TCE-PE
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @param limite_inferior Data limite inferior do ano dos contratos
+#' @param limite_superior Data limite superior do ano dos contratos
+#' @return Dataframe contendo informações sobre aditivos dos contratos
+#' @rdname fetch_aditivos_pe
+#' @examples
+#' aditivos_contratos_pe <- fetch_aditivos_pe(tce_bd_con)
+fetch_aditivos_pe <- function(tce_bd_con, limite_inferior = 2017, limite_superior = 2020) {
+  
+  aditivos <- tibble::tibble()
+  tryCatch({
+    aditivos <- DBI::dbGetQuery(tce_bd_con, sprintf(
+      "SELECT * FROM TERMOADITIVO WHERE AnoContrato >= %d AND AnoContrato <= %d;", limite_inferior, limite_superior))
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados dos aditivos (SQL Server): ", e))
+  )
+  
+  return(aditivos)
+}
+
+#' @title Busca dados dos municípios do TCE-PE
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @return Dataframe contendo informações sobre os municipios
+#' @rdname fetch_municipios_pe
+#' @examples
+#' municipios_pe <- fetch_municipios_pe(tce_bd_con)
+fetch_municipios_pe <- function(tce_bd_con) {
+  
+  municipios <- tibble::tibble()
+  tryCatch({
+    municipios <- DBI::dbGetQuery(tce_bd_con, "SELECT * FROM MUNICIPIO")
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados dos esquemas do Banco do TCE-PE: ", e))
+  )
+  
+  return(municipios)
+}
+
+#' @title Busca dados das unidades gestoras estaduais do TCE-PE
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @return Dataframe contendo informações sobre as unidades gestoras
+#' @rdname fetch_unidades_gestoras_estaduais_pe
+#' @examples
+#' ugs_estaduais <- fetch_unidades_gestoras_estaduais_pe(tce_bd_con)
+fetch_unidades_gestoras_estaduais_pe <- function(tce_bd_con) {
+  
+  unidades_gestoras <- tibble::tibble()
+  tryCatch({
+    unidades_gestoras <- DBI::dbGetQuery(tce_bd_con, "SELECT * FROM SCA_UJsEstaduais")
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados das unidades gestoras do Banco do TCE-PE: ", e))
+  )
+  
+  return(unidades_gestoras)
+}
+
+#' @title Busca dados das unidades gestoras municipais do TCE-PE
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @return Dataframe contendo informações sobre as unidades gestoras
+#' @rdname fetch_unidades_gestoras_municipais_pe
+#' @examples
+#' ugs_municipais <- fetch_unidades_gestoras_municipais_pe(tce_bd_con)
+fetch_unidades_gestoras_municipais_pe <- function(tce_bd_con) {
+  
+  unidades_gestoras <- tibble::tibble()
+  tryCatch({
+    unidades_gestoras <- DBI::dbGetQuery(tce_bd_con, "SELECT * FROM SCA_UJsMunicipais")
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados das unidades gestoras do Banco do TCE-PE: ", e))
+  )
+  
+  return(unidades_gestoras)
+}
+
+#' @title Busca dados das fornecedores do TCE-PE
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @return Dataframe contendo informações dos fornecedores
+#' @rdname fetch_fornecedores_pe
+#' @examples
+#' fornecedores <- fetch_fornecedores_pe(tce_bd_con)
+fetch_fornecedores_pe <- function(tce_bd_con) {
+  
+  fornecedores <- tibble::tibble()
+  tryCatch({
+    fornecedores <- DBI::dbGetQuery(tce_bd_con, "SELECT * FROM SCA_Pessoa")
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados dos fornecedores do Banco do TCE-PE: ", e))
+  )
+  
+  return(fornecedores)
+}
+
+#' @title Busca dados das tabelas do banco TCE-PE SQLServer
+#' @param tce_bd_con Conexão com o banco do TCE-PE
+#' @return Dataframe contendo informações das tabelas do banco
+#' @rdname lista_esquemas_tce_pe
+#' @examples
+#' info_tabelas <- lista_esquemas_tce_pe(tce_bd_con)
+lista_esquemas_tce_pe <- function(tce_bd_con) {
+  
+  esquemas <- tibble::tibble()
+  tryCatch({
+    esquemas <- DBI::dbGetQuery(tce_bd_con, "SELECT * FROM sys.Tables GO")
+  }, 
+  error = function(e) print(paste0("Erro ao buscar dados dos esquemas do Banco do TCE-PE: ", e))
+  )
+  
+  return(esquemas)
+}
---
+++ code/fetcher/scripts/fetch_dados_tce_pe.R
@@ -0,0 +1,74 @@
+library(magrittr)
+
+source(here::here("code/fetcher/setup/constants.R"))
+source(here::here("code/fetcher/DAO_TCE_PE.R"))
+
+tce_pe_con <- NULL
+
+.HELP <- "Rscript fetch_dados_tce_pe.R --data_inicio <2017> --data_fim <2020>"
+
+
+#' @title Obtém argumentos passados por linha de comando
+get_args <- function() {
+  args = commandArgs(trailingOnly=TRUE)
+  
+  option_list = list(
+    optparse::make_option(c("--data_inicio"),
+                          type="integer",
+                          default=2017,
+                          help="Data inicial da seleção (para licitações e contratos).",
+                          metavar="integer"),
+    optparse::make_option(c("--data_fim"),
+                          type="integer",
+                          default=2020,
+                          help="Data final da seleção (para licitações e contratos).",
+                          metavar="integer")
+  );
+  
+  opt_parser <- optparse::OptionParser(option_list = option_list, usage = .HELP)
+  
+  opt <- optparse::parse_args(opt_parser)
+  return(opt);
+}
+
+args <- get_args()
+
+data_inicio <- args$data_inicio
+data_fim <- args$data_fim
+
+tryCatch(
+  {tce_pe_con <- DBI::dbConnect(odbc::odbc(),
+                                Driver = "ODBC Driver 17 for SQL Server",
+                                Database = SQLSERVER_TCE_PE_DATABASE,
+                                Server = paste0(SQLSERVER_TCE_PE_HOST,",", SQLSERVER_TCE_PE_PORT),
+                                UID = SQLSERVER_TCE_PE_USER,
+                                PWD = SQLSERVER_TCE_PE_PASS)
+  cat("-  Acesso ao TCE-PE criado com sucesso!\n")
+  }, 
+  error = function(e) stop(paste0("Erro ao tentar se conectar ao Banco do TCE-PE: ",e))
+)
+
+tabelas <- lista_esquemas_tce_pe(tce_pe_con)
+
+municipios <- fetch_municipios_pe(tce_pe_con)
+
+licitacoes <- fetch_licitacoes_pe(tce_pe_con, data_inicio, data_fim)
+
+contratos <- fetch_contratos_pe(tce_pe_con, data_inicio, data_fim)
+
+aditivos <- fetch_aditivos_pe(tce_pe_con, data_inicio, data_fim)
+
+ugs_estaduais <- fetch_unidades_gestoras_estaduais_pe(tce_pe_con)
+
+ugs_municipais <- fetch_unidades_gestoras_municipais_pe(tce_pe_con)
+
+fornecedores <- fetch_fornecedores_pe(tce_pe_con)
+
+readr::write_csv(municipios, "data/tce_pe/municipios.csv")
+readr::write_csv(licitacoes, "data/tce_pe/licitacoes.csv")
+readr::write_csv(contratos, "data/tce_pe/contratos.csv")
+readr::write_csv(aditivos, "data/tce_pe/aditivos.csv")
+readr::write_csv(ugs_estaduais, "data/tce_pe/ugs_estaduais.csv")
+readr::write_csv(ugs_municipais, "data/tce_pe/ugs_municipais.csv")
+readr::write_csv(fornecedores, "data/tce_pe/fornecedores.csv")
+
