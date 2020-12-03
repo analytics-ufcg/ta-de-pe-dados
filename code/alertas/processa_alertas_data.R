2d13e0ab18e3c58b84f438d7038b15ae42e47f03
Joaquim Honório <joaquimhonorio@copin.ufcg.edu.br>
2020-12-03 08:24:58 -0300

    Ajuste descrições e método para gerar alerta de itens

--- code/alertas/processa_alertas_data.R
+++ code/alertas/processa_alertas_data.R
@@ -11,7 +11,7 @@ source(here::here("code/fetcher/scripts/fetcher_ta_na_mesa.R"))
 #' alertas <- create_tipo_alertas()
 create_tipo_alertas <- function() {
   id_tipo <- c(1,2)
-  titulo <- c("Contratado logo após a abertura.", "CNAE principal atípico no fornecimento do item.")
+  titulo <- c("Contratado logo após a abertura.", "Produtos atípicos.")
   
   tipos_alertas <- data.frame(id_tipo, titulo)
 }
@@ -22,27 +22,40 @@ create_tipo_alertas <- function() {
 #' @return Dataframe com alertas para CNAEs atípicos
 #' 
 #' @examples 
-#' alertas <- processa_alertas_cnaes_atipicos_itens(c(2018, 2019, 2020))
-processa_alertas_cnaes_atipicos_itens <- function(anos) {
-  print("Processando alertas de CNAEs atípicos por item...")
-  LIMITE_MIN_FREQUENCIA_CNAE = .01
+#' alertas <- processa_alertas_cnaes_atipicos_itens()
+processa_alertas_cnaes_atipicos_itens <- function() {
+  print("Processando alertas de itens atípicos por atividade econômica...")
+  LIMITE_MIN_PROP_CNAE = .01
   
   cnaes_itens_forcenedor <- .processa_itens_cnaes_fornecedores()
   
   cnaes_atipicos_data <- cnaes_itens_forcenedor %>% 
     group_by(id_contrato, razao_social, nr_documento_contratado, item_class) %>% 
-    arrange(desc(perc)) %>% 
-    mutate(max_perc = max(perc)) %>% 
+    arrange(desc(prop_grupo_total_item)) %>% 
+    mutate(max_prop_total_item = max(prop_grupo_total_item)) %>% 
     filter(is_cnae_fiscal == 't') %>% 
     mutate(id_tipo = 2) %>% 
     mutate(nr_documento = nr_documento_contratado) %>% 
-    mutate(info=paste0("A empresa ", razao_social," não possui nenhuma atividade econômica frequente para o item ", 
-                       ds_item," no contrato ",nr_contrato, "/", ano_contrato, " em ", nm_orgao,".")) %>% 
-    filter(max_perc <= LIMITE_MIN_FREQUENCIA_CNAE) %>% 
-    ungroup()%>% 
-    select("nr_documento", "id_contrato",	"id_tipo", "info")
-  
-  return(cnaes_atipicos_data)
+    mutate(atipico = max_prop_total_item <= LIMITE_MIN_PROP_CNAE) %>% 
+    filter(atipico) %>% 
+    generate_hash_id(c("id_contrato", "id_item_contrato", "id_tipo"), ITEM_ATIPICO) %>% 
+    generate_hash_id(c("id_tipo", "nr_documento", "id_contrato"), ALERTA_ID) %>% 
+    dplyr::select(id_item_atipico, id_alerta, id_item_contrato, id_contrato, nr_documento, id_tipo, ds_item) 
+    
+  contratos_itens_atipicos <- cnaes_atipicos_data %>%
+    group_by(id_contrato, nr_documento, id_tipo) %>% 
+    summarise(total_itens_atipicos=n(), .groups = 'drop') %>% 
+    mutate(info=ifelse(total_itens_atipicos != 1, 
+                       paste0("A empresa forneceu ", total_itens_atipicos, 
+                              " produtos que não são comuns com base em suas atividades econômicas declaradas na Receita Federal"),
+                       paste0("A empresa forneceu ", total_itens_atipicos, 
+                              " produto que não é comum com base em suas atividades econômicas declaradas na Receita Federal"))) %>% 
+    ungroup() %>% 
+    select(nr_documento, id_contrato, id_tipo, info)
+    
+  readr::write_csv(cnaes_atipicos_data %>% select(-c(nr_documento, id_tipo)), here::here("data/bd/itens_atipicos.csv"))
+    
+  return(contratos_itens_atipicos)
 }
 
 #' Processa alertas de fornecedores com relação a diferença entre a data de abertura e a data do primeiro contrato
@@ -169,11 +182,11 @@ processa_alertas_data_abertura_contrato <- function(anos) {
     left_join(itens_separated %>% select(c(id_item_contrato, item_class)), by="id_item_contrato")
   
   total_item_cnae_df <- cnae_fiscal_itens_unicos %>% 
-    select (item_class,  nm_cnae) %>% 
-    group_by(item_class, nm_cnae) %>% 
-    mutate(qt_total_item_cnae = n())%>%
+    select (item_class,  nm_grupo) %>% 
+    group_by(item_class, nm_grupo) %>% 
+    mutate(qt_total_item_grupo = n())%>%
     unique() %>% 
-    arrange(desc(qt_total_item_cnae))
+    arrange(desc(qt_total_item_grupo))
   
   total_item_df <- cnae_fiscal_itens_unicos %>% 
     select (item_class) %>% 
@@ -184,16 +197,14 @@ processa_alertas_data_abertura_contrato <- function(anos) {
   
   cnaes_itens_forcenedor <- cnae_all_itens_unicos %>% 
     left_join(total_item_df, by="item_class") %>%
-    left_join(total_item_cnae_df, by=c("nm_cnae", "item_class")) %>% 
-    mutate(perc= qt_total_item_cnae/qt_total_item) %>%
-    arrange(desc(perc)) %>% 
+    left_join(total_item_cnae_df, by=c("nm_grupo", "item_class")) %>% 
+    mutate(prop_grupo_total_item= qt_total_item_grupo/qt_total_item) %>%
     select("id_cnae", "id_item_contrato", "id_contrato", "ds_item", "nr_contrato", "ano_contrato", "nm_orgao", 
            "razao_social", "nr_documento_contratado", "is_cnae_fiscal", 
-           "nm_cnae", "item_class","qt_total_item", 
-           "qt_total_item_cnae", "perc") %>%
+           "nm_grupo","nm_divisao", "item_class","qt_total_item", 
+           "qt_total_item_grupo", "prop_grupo_total_item") %>%
     mutate_all(funs(ifelse(is.na(.), 0, .))) %>% 
-    filter(qt_total_item>=10)
+    filter(qt_total_item>=500)
   
   return(cnaes_itens_forcenedor)  
-
 }
--- code/processor/export_alertas_bd.R
+++ code/processor/export_alertas_bd.R
@@ -25,7 +25,7 @@ print("Criando alertas...")
 tipos_alerta <- create_tipo_alertas()
 
 alertas_data <- processa_alertas_data_abertura_contrato(anos)
-alertas_cnae_atipico_item <- processa_alertas_cnaes_atipicos_itens(anos)
+alertas_cnae_atipico_item <- processa_alertas_cnaes_atipicos_itens()
 
 alertas <- bind_rows(alertas_data, alertas_cnae_atipico_item)
 
