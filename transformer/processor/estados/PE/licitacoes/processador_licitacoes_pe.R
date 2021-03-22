5491b1fd1cb291164280940b963aea22fe23939f
gileadekelvin <gileadekelvin@gmail.com>
2021-03-22 17:24:30 -0300

    Adiciona parâmetro do filtro

--- transformer/processor/estados/PE/licitacoes/processador_licitacoes_pe.R
+++ transformer/processor/estados/PE/licitacoes/processador_licitacoes_pe.R
@@ -2,11 +2,13 @@ source(here::here("transformer/adapter/estados/PE/licitacoes/adaptador_licitacoe
 
 #' Processa dados das licitações do estado de Pernambuco
 #' 
+#' @param filtro Filtro do assunto para licitações
+#' 
 #' @return Dataframe com informações processadas das licitações
 #' 
 #' @examples 
 #' licitacoes_pe <- processa_licitacoes_pe()
-processa_licitacoes_pe <- function() {
+processa_licitacoes_pe <- function(filtro) {
   licitacoes_pe <- import_licitacoes_pe() %>%
     adapta_info_licitacoes_pe(tipo_filtro = filtro) %>%
     add_info_estado(sigla_estado = "PE", id_estado = "26")
