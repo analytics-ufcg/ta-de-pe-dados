1027607fe97911e81e17f003943cb5b866881b58
felipe <felipe.siqueira@ccc.ufcg.edu.br>
2021-09-02 22:47:06 -0300

    Corrigindo questões pontuadas na revisão

--- feed/scripts/import/import_empenhos_federais.sql
+++ feed/scripts/import/import_empenhos_federais.sql
@@ -1,5 +1,5 @@
 
 SET datestyle = ymd;
-\copy empenho_raw FROM '/data/dados_federais/despesa_empenho.csv' WITH NULL AS '' DELIMITER ',' CSV HEADER;
-\copy empenho_raw FROM '/data/dados_federais/despesa_item_empenho.csv' WITH NULL AS '' DELIMITER ',' CSV HEADER;
+\copy empenhos_raw_federais FROM '/data/dados_federais/despesa_empenho.csv' WITH NULL AS '' DELIMITER ',' CSV HEADER;
+\copy itens_empenhos_raw_federais FROM '/data/dados_federais/despesa_item_empenho.csv' WITH NULL AS '' DELIMITER ',' CSV HEADER;
 
