b47c69210edc7ed40eb9373160f11c570e7a9fd9
felipe <felipe.siqueira@ccc.ufcg.edu.br>
2021-09-10 16:11:42 -0300

    implementando task 842 e formatando read_empenhos_federais

--- transformer/utils/read/read_empenhos_federais.R
+++ transformer/utils/read/read_empenhos_federais.R
@@ -3,31 +3,32 @@ library(tidyverse)
 library(here)
 library(magrittr)
 
-help <- "
-Usage:
-Rscript export_empenhos_federais.R
-"
-
-host <- Sys.getenv("POSTGRES_HOST")
-user <- Sys.getenv("POSTGRES_USER")
-database <- Sys.getenv("POSTGRES_DB")
-port <- Sys.getenv("POSTGRES_PORT")
-password <- Sys.getenv("POSTGRES_PASSWORD")
-
-message(paste0("Host: ", host))
-message(paste0("User: ", user))
-
 source(here::here("transformer/utils/utils.R"))
 source(here::here("transformer/utils/join_utils.R"))
 source(here::here("transformer/utils/constants.R"))
 
-con <- DBI::dbConnect(RPostgres::Postgres(),
+help <- "
+Usage:
+Rscript export_empenhos_federais.R
+"
+read_empenhos_federais <- function(){
+  host <- Sys.getenv("POSTGRES_HOST")
+  user <- Sys.getenv("POSTGRES_USER")
+  database <- Sys.getenv("POSTGRES_DB")
+  port <- Sys.getenv("POSTGRES_PORT")
+  password <- Sys.getenv("POSTGRES_PASSWORD")
+  
+  message(paste0("Host: ", host))
+  message(paste0("User: ", user))
+  
+  con <- DBI::dbConnect(RPostgres::Postgres(),
                       dbname = database, 
                       host = host, 
                       port = port,
                       user = user,
                       password = 'secret')
-
-res <- DBI::dbGetQuery(con, "SELECT * FROM empenhos_raw_federais WHERE acao NOT IN ('00S4', '00S5', '00S7', '00S8', '00S9', '00SF', '00SH', '21C0', '21C1', '21C2', '00SI', '21C0')")
-
-return(res)
+  
+  res <- DBI::dbGetQuery(con, "SELECT * FROM empenhos_raw_federais WHERE acao NOT IN ('00S4', '00S5', '00S7', '00S8', '00S9', '00SF', '00SH', '21C0', '21C1', '21C2', '00SI', '21C0')")
+  
+  return(res)
+}
