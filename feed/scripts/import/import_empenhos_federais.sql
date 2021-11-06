45a616bfa0d9260d0f67d8dd6a8b362322433f29
gileadekelvin <gileadekelvin@gmail.com>
2021-11-06 15:03:01 -0300

    Adiciona tabela do histórico de itens federais

--- feed/scripts/import/import_empenhos_federais.sql
+++ feed/scripts/import/import_empenhos_federais.sql
@@ -1,3 +1,4 @@
 SET datestyle = ymd;
 \copy empenhos_raw_federais FROM PROGRAM 'gzip -dc /data/dados_federais/despesa_empenho.csv.gz' WITH NULL AS '' DELIMITER ',' CSV HEADER;
 \copy itens_empenhos_raw_federais FROM PROGRAM 'gzip -dc /data/dados_federais/despesa_item_empenho.csv.gz' WITH NULL AS '' DELIMITER ',' CSV HEADER;
+\copy itens_historico_raw_federais FROM PROGRAM 'gzip -dc /data/dados_federais/despesa_item_historico.csv.gz' WITH NULL AS '' DELIMITER ',' CSV HEADER;
