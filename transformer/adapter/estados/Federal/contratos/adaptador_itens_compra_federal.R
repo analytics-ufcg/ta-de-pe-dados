3ae833019cb46c8691578019e42a29786e15f7a0
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2021-12-02 20:05:52 -0300

    move if para mutate e cria coluna valor_original

--- transformer/adapter/estados/Federal/contratos/adaptador_itens_compra_federal.R
+++ transformer/adapter/estados/Federal/contratos/adaptador_itens_compra_federal.R
@@ -72,8 +72,9 @@ atualiza_preco_itens_federais <- function(itens_compra_federal_df, historico_ite
                select(codigo_empenho, sequencial, valor_atual), 
                by = c("codigo_empenho", "sequencial")) %>% 
     group_by(codigo_empenho, sequencial) %>% 
+    mutate(quantidade = if_else(tipo_operacao == 'ANULACAO', quantidade * -1, quantidade),
+           valor_original = valor_atual) %>% 
     summarise(
-      quantidade = if_else(tipo_operacao == 'ANULACAO', quantidade * -1, quantidade),
       quantidade = sum(quantidade),
       valor_unitario = if_else(quantidade == 0 || valor_unitario == 0, 0, mean(valor_unitario)),
       valor_atual = (valor_atual * quantidade)
