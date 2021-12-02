fe1cfac460d843277d066fda9979d393756c4359
Jefferson Neves <jefferson.rpn@gmail.com>
2021-12-02 11:06:01 -0300

    Adiciona descrição aos tipos de alerta

--- feed/scripts/create/create_tipo_alerta.sql
+++ feed/scripts/create/create_tipo_alerta.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS tipo_alerta;
 CREATE TABLE IF NOT EXISTS "tipo_alerta" ( 
     "id_tipo" INTEGER,
     "titulo" VARCHAR(240),
+    "descricao" VARCHAR(240),
     PRIMARY KEY("id_tipo")
     
 );
