e916168ac24a48efc0a91e85a9edfc227b1b4c14
gileadekelvin <gileadekelvin@gmail.com>
2021-12-04 10:43:27 -0300

    Ajusta cálculo do histórico de itens

--- transformer/adapter/estados/Federal/contratos/adaptador_itens_compra_federal.R
+++ transformer/adapter/estados/Federal/contratos/adaptador_itens_compra_federal.R
@@ -72,13 +72,15 @@ atualiza_preco_itens_federais <- function(itens_compra_federal_df, historico_ite
                select(codigo_empenho, sequencial, valor_atual), 
                by = c("codigo_empenho", "sequencial")) %>% 
     mutate(quantidade = if_else(tipo_operacao == 'ANULACAO', -(quantidade), quantidade)) %>% 
+    ungroup() %>% 
+    mutate(valor_original = valor_atual) %>% 
     group_by(codigo_empenho, sequencial) %>% 
     summarise(
       quantidade = sum(quantidade),
-      valor_unitario = if_else(quantidade == 0 || valor_unitario == 0, 0, mean(valor_unitario)),
-      valor_original = valor_atual,
-      valor_atual = (valor_unitario * quantidade)
+      valor_unitario = mean(valor_unitario),
+      valor_original = first(valor_original)
     ) %>%
+    mutate(valor_atual = valor_unitario * quantidade) %>% 
     ungroup() %>%
     mutate(tem_alteracoes = TRUE)
   
