f685b6d5f48507ba2f38ac4b9baee47ed9aac6da
felipe <felipe.siqueira@ccc.ufcg.edu.br>
2021-12-10 15:56:22 -0300

    conclusão da task de atualização dos valores dos contratos federais

--- transformer/processor/export_atualiza_contratos_bd.R
+++ transformer/processor/export_atualiza_contratos_bd.R
@@ -8,10 +8,11 @@ source(here::here("transformer/utils/read_utils.R"))
 source(here::here("transformer/adapter/estados/Federal/contratos/adaptador_compras_federal.R"))
 
 contratos_processados_df <- read_contratos_processados()
-empenhos_federais <- import_empenhos_federal() %>% select(codigo, valor_original)
+empenhos_federais <- import_empenhos_federal() %>% select(valor_original, codigo)
 
 filtra_contratos_tem_alteracoes <- function(contratos_processados_df){
-  contratos_filtrados <- contratos_processados_df %>% filter(tem_alteracoes == TRUE & id_estado == 99)
+  contratos_filtrados <- contratos_processados_df %>% filter(tem_alteracoes == TRUE & id_estado == 99) %>% head(n=50)
+  contratos_processados_df <- contratos_processados_df %>% filter(tem_alteracoes == FALSE | tem_alteracoes == TRUE & id_estado != 99)
   
   return(contratos_filtrados)
 }
@@ -20,6 +21,7 @@ empenhos_relacionados <- data.frame(Data=character(),
                                     Fase=character(), 
                                     Documento=character(), 
                                     Espécie=character(), 
+                                    codigo_empenho_original=character(), 
                                     stringsAsFactors=FALSE) 
 
 gera_df_documentos_relacionados <- function(documentos_relacionados){
@@ -31,8 +33,45 @@ gera_df_documentos_relacionados <- function(documentos_relacionados){
 
 contratos_filtrados <- filtra_contratos_tem_alteracoes(contratos_processados_df)
 
+# Atribui a variável 'empenhos_relacionados' TODOS os empenhos relacionados baixados
+# referentes aos empenhos passados pelos contratos do Governo Federal que tem alteração.
 contratos_filtrados$codigo_contrato %>% 
   map(fetch_documentos_relacionados_federais) %>% 
   map(gera_df_documentos_relacionados)
 
-write_csv2(empenhos_relacionados, "./data/empenhos.csv")
+# Torna os valores das anulações negativas e soma os valores de empenhos relacionados que estejam
+# relacionados ao mesmo empenho original.
+empenhos_relacionados <- empenhos_relacionados %>% filter(Fase == "Empenho") %>%
+  rename(codigo = Documento) %>%
+  select(-X5) %>%
+  left_join(empenhos_federais, by="codigo") %>% 
+  mutate(valor_original = ifelse(Espécie == "ANULAÇÃO", valor_original * -1, valor_original),
+         ) %>%
+  group_by(codigo_empenho_original) %>%
+  mutate(alteracoes = sum(valor_original)) %>%
+  group_by(codigo_empenho_original) %>%
+  filter(! duplicated(codigo_empenho_original))
+
+# Atualiza na tabela empenhos_relacionados o valor dos contratos somando as alterações com o valor original
+empenhos_relacionados <- empenhos_federais %>%
+  select(codigo, valor_original) %>%
+  rename(codigo_empenho_original = codigo,
+         valor_empenho_original = valor_original) %>%
+  right_join(empenhos_relacionados, by="codigo_empenho_original") %>%
+  mutate(valor_final = valor_empenho_original + alteracoes) %>%
+  select(codigo_empenho_original, valor_empenho_original, alteracoes, valor_final)
+  
+
+# Leva o valor atualizado dos empenhos_relacionados para a tabela de contratos_filtrados e depois devolve
+# essa amostra atualizada para a tabela de onde a amostra havia sido retirada.
+contratos_filtrados <- contratos_filtrados %>%
+  right_join(empenhos_relacionados %>% 
+               rename(codigo_contrato = codigo_empenho_original) %>%
+               select(codigo_contrato, valor_final), by="codigo_contrato") %>%
+  mutate(vl_contrato = valor_final) %>%
+  select(-valor_final) %>%
+  full_join(contratos_processados_df)
+
+               
+
+write_csv2(contratos_filtrados, "./data/bd/info_contrato.csv")
--- transformer/utils/fetcher/export_documentos_federais_relacionados.R
+++ transformer/utils/fetcher/export_documentos_federais_relacionados.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(futile.logger)
+library(dplyr)
 
 fetch_documentos_relacionados_federais <- function(codigo) {
   url <- str_glue("http://www.portaltransparencia.gov.br/despesas/documento/documentos-relacionados/baixar?direcaoOrdenacao=asc&codigo={codigo}&fase=Empenho")
@@ -10,6 +11,8 @@ fetch_documentos_relacionados_federais <- function(codigo) {
   tryCatch({
     download.file(url, temp)
     dados <- read_csv2(temp)
+    dados <- dados %>%
+      mutate(codigo_empenho_original = codigo) 
     unlink(temp)
   }, error = function(e) {
     flog.info("Não foi possível realizar o download dos dados! Verifique se o download está disponível no Portal de Transparência")
